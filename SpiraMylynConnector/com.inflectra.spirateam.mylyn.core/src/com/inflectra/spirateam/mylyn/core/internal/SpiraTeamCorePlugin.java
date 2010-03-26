75d54e4e840f55f0a17366dddb612df7db1fa364
asandman <asandman@5d4b435c-4d46-d949-aab2-e601ca53af14>
2010-03-26 14:36:46 +0000

    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@10714 5d4b435c-4d46-d949-aab2-e601ca53af14

--- SpiraMylynConnector/com.inflectra.spirateam.mylyn.core/src/com/inflectra/spirateam/mylyn/core/internal/SpiraTeamCorePlugin.java
+++ SpiraMylynConnector/com.inflectra.spirateam.mylyn.core/src/com/inflectra/spirateam/mylyn/core/internal/SpiraTeamCorePlugin.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2010 Inflectra Corporation.
+ * Inflectra, SpiraTest, SpiraPlan and SpiraTeam are either registered trademarks
+ * or trademarks of Inflectra Corporation in the U.S. and/or other countries.
+ *******************************************************************************/
+
 package com.inflectra.spirateam.mylyn.core.internal;
 
 import org.eclipse.core.runtime.Plugin;
@@ -6,25 +12,28 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class SpiraCorePlugin extends Plugin {
+public class SpiraTeamCorePlugin extends Plugin
+{
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.inflectra.spira.mylyn.core";
 
 	// The shared instance
-	private static SpiraCorePlugin plugin;
+	private static SpiraTeamCorePlugin plugin;
 	
 	/**
 	 * The constructor
 	 */
-	public SpiraCorePlugin() {
+	public SpiraTeamCorePlugin()
+	{
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext context) throws Exception {
+	public void start(BundleContext context) throws Exception
+	{
 		super.start(context);
 		plugin = this;
 	}
@@ -33,7 +42,8 @@ public class SpiraCorePlugin extends Plugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext context) throws Exception {
+	public void stop(BundleContext context) throws Exception
+	{
 		plugin = null;
 		super.stop(context);
 	}
@@ -43,7 +53,8 @@ public class SpiraCorePlugin extends Plugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static SpiraCorePlugin getDefault() {
+	public static SpiraTeamCorePlugin getDefault()
+	{
 		return plugin;
 	}
 

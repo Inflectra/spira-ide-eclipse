78b475eb8f48c86d557699a0d4504698faa7726f
asandman <asandman@5d4b435c-4d46-d949-aab2-e601ca53af14>
2010-03-26 14:37:57 +0000

    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@10715 5d4b435c-4d46-d949-aab2-e601ca53af14

--- SpiraMylynConnector/com.inflectra.spirateam.mylyn.ui/src/com/inflectra/spirateam/mylyn/ui/internal/SpiraTeamUiPlugin.java
+++ SpiraMylynConnector/com.inflectra.spirateam.mylyn.ui/src/com/inflectra/spirateam/mylyn/ui/internal/SpiraTeamUiPlugin.java
@@ -1,4 +1,4 @@
-package com.inflectra.spirateam.mylyn.ui;
+package com.inflectra.spirateam.mylyn.ui.internal;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;

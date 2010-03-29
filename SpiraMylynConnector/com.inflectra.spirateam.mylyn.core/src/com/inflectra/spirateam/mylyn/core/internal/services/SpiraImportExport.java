c2bdccdb7f945da7de3a7df5b1762f702c8ec55d
asandman <asandman@5d4b435c-4d46-d949-aab2-e601ca53af14>
2010-03-29 21:48:46 +0000

    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@10740 5d4b435c-4d46-d949-aab2-e601ca53af14

---
+++ SpiraMylynConnector/com.inflectra.spirateam.mylyn.core/src/com/inflectra/spirateam/mylyn/core/internal/services/SpiraImportExport.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.inflectra.spirateam.mylyn.core.internal.services;
+
+import com.inflectra.spirateam.mylyn.core.internal.services.soap.*;
+
+/**
+ * This is a facade over the auto-generated proxy class
+ * that simplifies some of the connection handling
+ * 
+ * @author Inflectra Corporation
+ */
+public class SpiraImportExport extends ImportExport
+{
+	/**
+	 * The constructor
+	 */
+	public SpiraImportExport()
+	{
+	}
+}

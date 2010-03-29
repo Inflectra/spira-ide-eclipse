bb80680d6682a481c220f16db9f38d83a5b71bca
asandman <asandman@5d4b435c-4d46-d949-aab2-e601ca53af14>
2010-03-29 22:13:10 +0000

    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@10741 5d4b435c-4d46-d949-aab2-e601ca53af14

--- SpiraMylynConnector/com.inflectra.spirateam.mylyn.core/src/com/inflectra/spirateam/mylyn/core/internal/services/SpiraImportExport.java
+++ SpiraMylynConnector/com.inflectra.spirateam.mylyn.core/src/com/inflectra/spirateam/mylyn/core/internal/services/SpiraImportExport.java
@@ -7,16 +7,52 @@ import com.inflectra.spirateam.mylyn.core.internal.services.soap.*;
 
 /**
  * This is a facade over the auto-generated proxy class
- * that simplifies some of the connection handling
+ * that simplifies the inner-workings of the SOAP/WSDL classes
  * 
  * @author Inflectra Corporation
  */
-public class SpiraImportExport extends ImportExport
+public class SpiraImportExport
 {
+	private static final String WEB_SERVICE_SUFFIX = "/Services/v2_2/ImportExport.asmx";
+	
+	private String serviceUrl = "";
+	private String userName = "";
+	private String password = "";
+	
 	/**
 	 * The constructor
 	 */
-	public SpiraImportExport()
+	public SpiraImportExport(String baseUrl)
 	{
+		//Set the web service URL
+		this.serviceUrl = baseUrl + WEB_SERVICE_SUFFIX;
 	}
+	
+	/**
+	 * The constructor
+	 */
+	public SpiraImportExport(String baseUrl, String userName, String password)
+	{
+		//Set the URL, username and password
+		this.serviceUrl = baseUrl + WEB_SERVICE_SUFFIX;
+		this.userName = userName;
+		this.password = password;
+	}
+
+	/**
+	 * The user name
+	 */
+	public String getUserName()
+	{
+		return this.userName;
+	}
+
+	/**
+	 * The password
+	 */
+	public String getPassword()
+	{
+		return this.password;
+	}
+	
 }

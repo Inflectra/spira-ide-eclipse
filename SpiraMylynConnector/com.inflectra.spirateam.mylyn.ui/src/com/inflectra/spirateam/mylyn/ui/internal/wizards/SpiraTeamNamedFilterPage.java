873abbcdef826312dbf778902fc9281f1e891a5a
asandman <asandman@5d4b435c-4d46-d949-aab2-e601ca53af14>
2010-03-31 20:56:45 +0000

    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@10760 5d4b435c-4d46-d949-aab2-e601ca53af14

--- SpiraMylynConnector/com.inflectra.spirateam.mylyn.ui/src/com/inflectra/spirateam/mylyn/ui/internal/wizards/SpiraTeamNamedFilterPage.java
+++ SpiraMylynConnector/com.inflectra.spirateam.mylyn.ui/src/com/inflectra/spirateam/mylyn/ui/internal/wizards/SpiraTeamNamedFilterPage.java
@@ -10,6 +10,9 @@ import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 
+import com.inflectra.spirateam.mylyn.core.internal.SpiraTeamUtil;
+import com.inflectra.spirateam.mylyn.core.internal.model.PredefinedFilter;
+
 /**
  * Wizard page that allows the user to select a predefined SpiraTeam filter.
  * Eventually it will also need to support creating filters and retrieving
@@ -18,6 +21,8 @@ import org.eclipse.swt.layout.GridLayout;
 public class SpiraTeamNamedFilterPage extends AbstractRepositoryQueryPage
 {
 	private List filterList;
+	
+	private PredefinedFilter[] filters = null;
 	
 	/**
 	 * Constructor method
@@ -42,18 +47,33 @@ public class SpiraTeamNamedFilterPage extends AbstractRepositoryQueryPage
 		setDescription(Messages.SpiraTeamNamedFilterPage_Please_select_named_queries);
 		setPageComplete(false);
 	}
-	
+		
+	/**
+	 * Applies the specified filter
+	 */
 	@Override
 	public void applyTo(IRepositoryQuery query)
 	{
-		// TODO Auto-generated method stub
-
+		PredefinedFilter filter = getSelectedFilter();
+		query.setSummary(filter.getName());
+		SpiraTeamUtil.setQuery(getTaskRepository(), query, filter);
 	}
 
 	@Override
 	public String getQueryTitle()
 	{
 		return Messages.SpiraTeamNamedFilterPage_Please_select_named_queries;
+	}
+	
+
+	/** Returns the filter selected by the user or null on failure */
+	private PredefinedFilter getSelectedFilter()
+	{
+		if (filters != null && filters.length > 0)
+		{
+			return filters[filterList.getSelectionIndex()];
+		}
+		return null;
 	}
 
 	/**
@@ -69,15 +89,28 @@ public class SpiraTeamNamedFilterPage extends AbstractRepositoryQueryPage
 		innerComposite.setLayout(gl);
 		
 		filterList = new List(innerComposite, SWT.V_SCROLL | SWT.BORDER);
-		filterList.add("Filter");
 		filterList.deselectAll();
 		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
 		data.horizontalIndent = 15;
 		filterList.setLayoutData(data);
 		filterList.setEnabled(true);
+	
+		int n = 0;
+		this.filters = SpiraTeamUtil.createPredefinedFilters(); 
+		for (int i = 0; i < filters.length; i++)
+		{
+			filterList.add(filters[i].getName());
+			/*if (filters[i].getId().equals(workingCopy.getId()))
+			{
+				n = i;
+			}*/
+		}
+
+		/*filterList.select(n);*/
+		filterList.showSelection();
 		
 		Dialog.applyDialogFont(innerComposite);
 		setControl(innerComposite);
+		//setPageComplete(true);
 	}
-
 }

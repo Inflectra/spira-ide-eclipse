e46501430e93b633adf1dff902ceadbbe29b1f68
asandman <asandman@5d4b435c-4d46-d949-aab2-e601ca53af14>
2010-03-31 20:56:35 +0000

    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@10759 5d4b435c-4d46-d949-aab2-e601ca53af14

---
+++ SpiraMylynConnector/com.inflectra.spirateam.mylyn.core/src/com/inflectra/spirateam/mylyn/core/internal/SpiraTeamUtil.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.inflectra.spirateam.mylyn.core.internal;
+
+import org.eclipse.mylyn.tasks.core.IRepositoryQuery;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+
+import com.inflectra.spirateam.mylyn.core.internal.model.Filter;
+import com.inflectra.spirateam.mylyn.core.internal.model.PredefinedFilter;;
+
+/**
+ * @author Inflectra Corporation
+ *
+ */
+public class SpiraTeamUtil
+{
+	private static final String KEY_FILTER_ID = "FilterID"; //$NON-NLS-1$
+	private static final String KEY_FILTER_NAME = "FilterName"; //$NON-NLS-1$
+	
+	/**
+	 * Sets the passed in pre-defined filter
+	 * @param taskRepository
+	 * @param query
+	 * @param filter
+	 */
+	public static void setQuery(TaskRepository taskRepository, IRepositoryQuery query, Filter filter)
+	{
+		if (filter instanceof PredefinedFilter)
+		{
+			PredefinedFilter predefinedFilter = (PredefinedFilter) filter;
+			query.setAttribute(KEY_FILTER_ID, predefinedFilter.getId());
+			query.setAttribute(KEY_FILTER_NAME, predefinedFilter.getName());
+		}
+	}
+	
+	public static PredefinedFilter[] createPredefinedFilters()
+	{
+		PredefinedFilter[] filters = new PredefinedFilter[3];
+		
+		//Setup the three predefined queries:
+		//My Assigned Requirements
+		//My Assigned Incidents
+		//My Assigned Tasks
+		filters[0] = new PredefinedFilter("1", "My Assigned Requirements");
+		filters[1] = new PredefinedFilter("2", "My Assigned Incidents");
+		filters[2] = new PredefinedFilter("3", "My Assigned Tasks");
+
+		return filters;
+	}
+}
